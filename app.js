ea342f47993d0c178b2d0ccccf29325346b3fc4c
Ariel Mannes <VFQK84@motorolasolutions.com>
2018-03-07 14:36:50 +0200

    added calculation in the js file

--- app.js
+++ app.js
@@ -4,10 +4,23 @@
 	angular.module('myFirstApp', [])
 	
 	.controller('MyFirstController', function($scope){
-		$scope.name = "null";
+		$scope.name = "";
+		$scope.totalVal = 0;
 		$scope.sayHello = function(){
 			return "Hello "+ $scope.name;
 		};
+		$scope.calcNameValue = function(){
+			var totalNameValue = calaculatNumercFromString($scope.name);
+			$scope.totalVal = totalNameValue;
+		};
 	});
+	
+	function calaculatNumercFromString(string){
+		var totalVal = 0;
+		for(var i = 0;i<string.length;i++){
+			totalVal += string.charCodeAt(i);
+		}
+		return totalVal;
+	}
 	
 })();
